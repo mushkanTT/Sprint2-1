fe6d840bd133753dd34a7a71709e1cf3059ab7ea
nikita2512 <75368548+nikita2512@users.noreply.github.com>
2020-12-26 10:51:45 +0530

    Update EmployeeServiceImpl.java

--- src/main/java/com/cg/bugtracking/service/EmployeeServiceImpl.java
+++ src/main/java/com/cg/bugtracking/service/EmployeeServiceImpl.java
@@ -3,6 +3,8 @@ package com.cg.bugtracking.service;
 import static com.cg.bugtracking.util.AppConstant.EMPLOYEE_NOT_FOUND_CONST;
 import static com.cg.bugtracking.util.AppConstant.OPERATION_FAILED_CONST;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,12 +27,20 @@ public class EmployeeServiceImpl implements EmployeeService{
 	@Autowired
 	private EmployeeRepository employeeRepository;
 	
+	private static Logger logger=LoggerFactory.getLogger(EmployeeServiceImpl.class);
 	
-	
-
+	/**
+	 * This method is used to create employee to the database.
+	 * 
+	 * @param employee This is the argument to createEmployee method
+	 * @exception OperationException On input employee is null.
+	 * @return object of created employee
+	 */	
 	@Transactional
 	@Override
 	public Employee createEmployee(Employee emp) {
+		logger.info("Enter EmployeeController :: method=createEmployee");
+		
 		Employee employeeObj = null;
 		try {
 			employeeObj = employeeRepository.save(emp);
@@ -38,14 +48,24 @@ public class EmployeeServiceImpl implements EmployeeService{
 		} catch (Exception e) {
 			throw new OperationFailedException(OPERATION_FAILED_CONST+ e.getMessage());
 		}
+		
+		logger.info("Exit EmployeeController :: method=createEmployee");
 		return employeeObj;
 	}
 	
 	
-
+	/**
+	 * This method is used to update employee to the database.
+	 * 
+	 * @param long and employee This is the argument to updateProduct method
+	 * @exception ResourceNotFoundException and OperationException On input employee id or employee object is null.
+	 * @return Updated employee object
+	 */
 	@Transactional
 	@Override
 	public Employee updateEmployee(long id, Employee employee) {
+		logger.info("Enter EmployeeController :: method=updateEmployee");
+		
 		Optional<Employee> employeeObj = null;
 		Employee updatedEmployee = null;
 		employeeObj = employeeRepository.findById(id);
@@ -66,14 +86,23 @@ public class EmployeeServiceImpl implements EmployeeService{
 			}
 
 		}
-
+		
+		logger.info("Exit EmployeeController :: method=updateEmployee");
 		return updatedEmployee;
 	}
 
 	
-	
+	/**
+	 * This method is used to delete employee to the database.
+	 * 
+	 * @param long This is the argument to deleteEmployee method
+	 * @exception RsourceNotFoundException and OperationException On input employee id is null.
+	 * @return object of deleted employee
+	 */
 	@Override
 	public Employee deleteEmployee(long id) {
+		logger.info("Enter EmployeeController :: method=deleteEmployee");
+		
 		Optional<Employee> employeeObj = employeeRepository.findById(id);
 		if (!employeeObj.isPresent()) {
 			
@@ -89,32 +118,48 @@ public class EmployeeServiceImpl implements EmployeeService{
 			}			
 		}
 		
+		logger.info("Exit EmployeeController :: method=deleteEmployee");
 		return employeeObj.get();
 	}
 
 	
-	
+	/**
+	 * This method is used to get list of employee from the database.
+	 * 
+	 * @param  There is no argument to getAllEmployee method
+	 * @return List<Employee>
+	 */
 	@Override
 	public Employee getEmployee(long id) {
+		logger.info("Enter EmployeeController :: method=getEmployee");
+		
 		Optional<Employee> employee = employeeRepository.findById(id);
 
 		if (!employee.isPresent())
 			throw new ResourceNotFoundException("Employee not found for this id: " + id);
-
+		
+		logger.info("Exit EmployeeController :: method=getEmployee");
 		return employee.get();
 	}
 	
 	
-
+	/**
+	 * This method is used to get list of employee from the database.
+	 * 
+	 * @param  There is no argument to getAllEmployee method
+	 * @return List<Employee>
+	 */
 	@Override
 	public List<Employee> getAllEmployees() {
+		logger.info("Enter EmployeeController :: method=getAllEmployees");
+		logger.info("Exit EmployeeController :: method=getAllEmployees");
 		return employeeRepository.findAll();
 	}
 	
 	
 
 
-
+	
 	@Override
 	public String employeeLogin(User euser) {
 		// TODO Auto-generated method stub
